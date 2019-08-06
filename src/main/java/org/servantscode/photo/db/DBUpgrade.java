849c0b49bcaf6a3f011f2bc098452ea1f30310de
Greg Leitheiser <greg@servantscode.org>
2019-08-06 14:06:52 -0500

    Distributed DB Upgrade infrastructure

---
+++ src/main/java/org/servantscode/photo/db/DBUpgrade.java
@@ -0,0 +1,24 @@
+package org.servantscode.photo.db;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.servantscode.commons.db.AbstractDBUpgrade;
+
+import java.sql.SQLException;
+
+public class DBUpgrade extends AbstractDBUpgrade {
+    private static final Logger LOG = LogManager.getLogger(DBUpgrade.class);
+
+    @Override
+    public void doUpgrade() throws SQLException {
+        LOG.info("Verifying database structures.");
+
+        if(!tableExists("photos")) {
+            LOG.info("-- Creating photos table");
+            runSql("CREATE TABLE photos (guid TEXT PRIMARY KEY, " +
+                                        "filetype TEXT, " +
+                                        "bytes BYTEA, " +
+                                        "org_id INTEGER references organizations(id) ON DELETE CASCADE)");
+        }
+    }
+}
